83e5f9f2fb339a3fadc00bf2c8796a3ea2d2da07
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-09-28 15:16:48 +0200

    put all breakpoints in same file

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ btn.addEventListener("click", (event) => {
   event.preventDefault();
   if (!regex.test(inputValue.value)) {
     error.style.display = "block";
-  }
-  else {
-    inputValue.value = '';
+  } else {
+    inputValue.value = "";
     error.style.display = "none";
   }
 });
