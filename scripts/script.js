f163fdc3ae36c4eaa6b40455439a4e355b80bc16
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-09-30 11:27:36 +0200

    btnHoverFix

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ let btn = document.querySelector('.btn');
 let error = document.querySelector('.error');
 let inputValue = document.querySelector('.input');
 
-let regex = /@/;
+let regex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 btn.addEventListener('click', (event) => {
     event.preventDefault();
